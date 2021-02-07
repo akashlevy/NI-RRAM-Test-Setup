8a4eb1173ad2998cf9d0161b9e16bb99f52816f8
Akash Levy <akashlevy@gmail.com>
2021-02-07 06:42:21 -0800

    Upload new bitstream

--- tests/process_bitstream.py
+++ tests/process_bitstream.py
@@ -2,6 +2,6 @@
 import numpy as np
 
 # Create bitstream
-data = np.loadtxt(open("4b_32w_1s/array_R_map_0.csv"), delimiter=",", dtype=np.int32).transpose().flatten()
+data = np.loadtxt(open("4b_32w_1s/array_R_map_1.csv"), delimiter=",", dtype=np.int32).transpose().flatten()
 data = np.vectorize(lambda x: 1-x)(data)
-np.savetxt('bitstream.csv', data, fmt='%d')
+np.savetxt('bitstream2.csv', data, fmt='%d')
