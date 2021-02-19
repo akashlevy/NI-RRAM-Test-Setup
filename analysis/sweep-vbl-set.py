d6443a9790166d7d35f2286529f336414ef356d9
Akash Levy <akashlevy@gmail.com>
2021-02-19 09:02:15 -0800

    SET stuff

---
+++ analysis/sweep-vbl-set.py
@@ -0,0 +1,54 @@
+# Import libraries
+import matplotlib.pyplot as plt, numpy as np, pandas as pd
+
+# Chip number
+CHIP = 9
+
+# Load sweep data
+cols = ['addr', 'VBL', 'VWL', 'VSL', 'PW', 'Ri', 'Rf']
+dtypes = {
+    'addr': np.int32,
+    'VWL': np.float64,
+    'VBL': np.float64,
+    'VSL': np.float64,
+    'PW': np.int32,
+    'Ri': np.float64,
+    'Rf': np.float64,
+}
+data = pd.read_csv(f"../data/sweep_vbl_set_chip{CHIP}.csv", names=cols, sep="\t", dtype=dtypes)
+
+# Convert units
+data['Ri'] = data['Ri']/1000 # kOhm
+data['Rf'] = data['Rf']/1000 # kOhm
+data['Gi'] = 1/data['Ri']*1000 # uS
+data['Gf'] = 1/data['Rf']*1000 # uS
+
+# Iterate, plot, and save
+for PW in [50, 10, 5, 1]:
+    # Select pulse width
+    gdata0 = data[data['PW'] == PW]
+    for VWL in [3, 2, 1.5, 1]:
+        # Select bitline voltage and group by address for individual lines
+        gdata = gdata0[gdata0['VWL'] == VWL]
+        gdata = gdata.groupby('addr')
+
+        # Plot resistance
+        plt.figure(figsize=(4,3))
+        plt.title(f'Rf @ PW={PW}us, VWL={VWL}V')
+        gdata.plot(x='VBL', y='Rf', ax=plt.gca(), legend=False)
+        plt.xlabel('VBL (V)')
+        plt.ylabel('Final Resistance (kOhm)')
+        plt.ylim(0, 100)
+        plt.tight_layout()
+        plt.savefig(f'figs/sweeps/sweep-vbl-set-Rf-pw{PW}-vwl{VWL}.pdf')
+
+        # Plot conductance
+        plt.figure(figsize=(4,3))
+        plt.title(f'Gf @ PW={PW}us, VWL={VWL}V')
+        gdata.plot(x='VBL', y='Gf', ax=plt.gca(), legend=False)
+        plt.xlabel('VBL (V)')
+        plt.ylabel('Final Conductance (uS)')
+        plt.ylim(0, 200)
+        plt.tight_layout()
+        plt.savefig(f'figs/sweeps/sweep-vbl-set-Gf-pw{PW}-vbl{VWL}.pdf')
+
